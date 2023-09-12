ec2a780d51170a238c32e66047f1b1dd841394f7
Amirul <amirul.naim@zuscoffee.com>
2023-09-12 11:58:42 +0800

    Add connector

---
+++ src/Connectors/MayaPayConnector.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Naimsolong\MayaPay\Connectors;
+
+use Saloon\Http\Connector;
+
+class MayaPayConnector extends Connector
+{
+    public function resolveBaseUrl(): string
+    {
+        return config('mayapay-sdk.production_ready') ? config('mayapay-sdk.hostname.production') : config('mayapay-sdk.hostname.sandbox');
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+			'accept' => 'application/json',
+		  	'content-type' => 'application/json',
+		];
+    }
+}
