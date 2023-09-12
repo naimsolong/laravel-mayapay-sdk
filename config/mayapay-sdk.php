7c302a727bf8e00c8de8d5f83d6b61820393a15b
Amirul <amirul.naim@zuscoffee.com>
2023-09-12 11:58:26 +0800

    Update config file

--- config/mayapay-sdk.php
+++ config/mayapay-sdk.php
@@ -2,5 +2,15 @@
 
 // config for Naimsolong/MayaPay
 return [
+    'key' => [
+        'public' => env('MAYAPAY_PUBLIC_KEY', ''),
+        'secret' => env('MAYAPAY_SECRET_KEY', ''),
+    ],
 
+    'hostname' => [
+        'sandbox' => env('MAYAPAY_SANDBOX_HOSTNAME', 'https://pg-sandbox.paymaya.com'),
+        'production' => env('MAYAPAY_PRODUCTION_HOSTNAME', 'https://pg.maya.ph'),
+    ],
+
+    'production_ready' => false,
 ];
